0d86f621f78dbea9150e6e4338e62396d14f2102
Pedro Gomes <pedrogomes94@gmail.com>
2019-01-10 21:12:43 +0000

    update API to use mostly ArrayBuffers

--- index.d.ts
+++ index.d.ts
@@ -9,15 +9,15 @@ declare module "react-native-simple-crypto" {
 
   export namespace AES {
     export function encrypt(
-      text: string,
-      key: string,
-      iv: string
-    ): Promise<string>;
+      text: ArrayBuffer,
+      key: ArrayBuffer,
+      iv: ArrayBuffer
+    ): Promise<ArrayBuffer>;
     export function decrypt(
-      ciphertext: string,
-      key: string,
-      iv: string
-    ): Promise<string>;
+      ciphertext: ArrayBuffer,
+      key: ArrayBuffer,
+      iv: ArrayBuffer
+    ): Promise<ArrayBuffer>;
   }
 
   export namespace SHA {
@@ -27,17 +27,20 @@ declare module "react-native-simple-crypto" {
   }
 
   export namespace HMAC {
-    export function hmac256(ciphertext: string, key: string): Promise<string>;
+    export function hmac256(
+      ciphertext: ArrayBuffer,
+      key: ArrayBuffer
+    ): Promise<ArrayBuffer>;
   }
 
   export namespace PBKDF2 {
     export function hash(
       password: string,
-      saltBase64: string,
+      salt: ArrayBuffer,
       iterations: number,
       keyLen: number,
       hash: "SHA1" | "SHA224" | "SHA256" | "SHA384" | "SHA512"
-    ): Promise<string>;
+    ): Promise<ArrayBuffer>;
   }
 
   export namespace RSA {
--- index.js
+++ index.js
@@ -1,26 +1,158 @@
 "use strict";
 
 import base64js from "base64-js";
+import hexLite from "hex-lite";
 import { NativeModules } from "react-native";
 
-function randomBytes(length) {
-  return new Promise((resolve, reject) => {
-    NativeModules.RNRandomBytes.randomBytes(length, function(err, base64) {
-      if (err) {
-        reject(err);
-      } else {
-        const result = base64js.toByteArray(base64).buffer;
-        resolve(result);
-      }
-    });
-  });
+function convertArrayBufferToUtf8(arrayBuffer) {
+  const array = new Uint8Array(arrayBuffer);
+  const chars = [];
+  let i = 0;
+
+  while (i < array.length) {
+    const byte = array[i];
+    if (byte < 128) {
+      chars.push(String.fromCharCode(byte));
+      i++;
+    } else if (byte > 191 && byte < 224) {
+      chars.push(
+        String.fromCharCode(((byte & 0x1f) << 6) | (array[i + 1] & 0x3f))
+      );
+      i += 2;
+    } else {
+      chars.push(
+        String.fromCharCode(
+          ((byte & 0x0f) << 12) |
+            ((array[i + 1] & 0x3f) << 6) |
+            (array[i + 2] & 0x3f)
+        )
+      );
+      i += 3;
+    }
+  }
+
+  const utf8 = chars.join("");
+  return utf8;
+}
+
+function convertUtf8ToArrayBuffer(utf8) {
+  const bytes = [];
+
+  let i = 0;
+  utf8 = encodeURI(utf8);
+  while (i < utf8.length) {
+    const byte = utf8.charCodeAt(i++);
+    if (byte === 37) {
+      bytes.push(parseInt(utf8.substr(i, 2), 16));
+      i += 2;
+    } else {
+      bytes.push(byte);
+    }
+  }
+
+  const array = new Uint8Array(bytes);
+  const arrayBuffer = array.buffer;
+  return arrayBuffer;
+}
+
+function convertArrayBufferToBase64(arrayBuffer) {
+  const byteArray = new Uint8Array(arrayBuffer);
+  const result = base64js.fromByteArray(byteArray);
+  return result;
+}
+
+function convertBase64ToArrayBuffer(base64) {
+  const result = base64js.toByteArray(base64).buffer;
+  return result;
+}
+
+function convertArrayBufferToHex(arrayBuffer) {
+  const result = hexLite.fromBuffer(arrayBuffer);
+  return result;
+}
+
+function convertHexToArrayBuffer(hex) {
+  const result = hexLite.toBuffer(hex);
+  return result;
 }
 
 export default {
-  AES: NativeModules.Aes,
+  AES: {
+    encrypt: function(textArrayBuffer, keyArrayBuffer, ivArrayBuffer) {
+      const textString = convertArrayBufferToUtf8(textArrayBuffer);
+      const keyHex = convertArrayBufferToHex(keyArrayBuffer);
+      const ivBase64 = convertArrayBufferToBase64(ivArrayBuffer);
+      return new Promise((resolve, reject) => {
+        NativeModules.Aes.encrypt(textString, keyHex, ivBase64)
+          .then(cipherTextBase64 => {
+            const result = convertBase64ToArrayBuffer(cipherTextBase64);
+            resolve(result);
+          })
+          .catch(error => reject(error));
+      });
+    },
+    decrypt: function(cipherTextArrayBuffer, keyArrayBuffer, ivArrayBuffer) {
+      const cipherTextBase64 = convertArrayBufferToBase64(
+        cipherTextArrayBuffer
+      );
+      const keyHex = convertArrayBufferToHex(keyArrayBuffer);
+      const ivBase64 = convertArrayBufferToBase64(ivArrayBuffer);
+      return new Promise((resolve, reject) => {
+        NativeModules.Aes.decrypt(cipherTextBase64, keyHex, ivBase64)
+          .then(textString => {
+            const result = convertUtf8ToArrayBuffer(textString);
+            resolve(result);
+          })
+          .catch(error => reject(error));
+      });
+    }
+  },
   SHA: NativeModules.Sha,
-  HMAC: NativeModules.Hmac,
-  PBKDF2: NativeModules.Pbkdf2,
+  HMAC: {
+    hmac256: function(textArrayBuffer, keyArrayBuffer) {
+      const textHex = convertArrayBufferToHex(textArrayBuffer);
+      const keyHex = convertArrayBufferToHex(keyArrayBuffer);
+      return new Promise((resolve, reject) => {
+        NativeModules.Hmac.hmac256(textHex, keyHex)
+          .then(signatureHex => {
+            const result = convertHexToArrayBuffer(signatureHex);
+            resolve(result);
+          })
+          .catch(error => reject(error));
+      });
+    }
+  },
+  PBKDF2: {
+    hash: function(password, saltArrayBuffer, iterations, keyLength, hash) {
+      const saltBase64 = convertArrayBufferToBase64(saltArrayBuffer);
+      return new Promise((resolve, reject) => {
+        NativeModules.Pbkdf2.hash(
+          password,
+          saltBase64,
+          iterations,
+          keyLength,
+          hash
+        )
+          .then(hashHex => {
+            const result = convertHexToArrayBuffer(hashHex);
+            resolve(result);
+          })
+          .catch(error => reject(error));
+      });
+    }
+  },
+
   RSA: NativeModules.Rsa,
-  randomBytes: randomBytes
+  randomBytes: function randomBytes(length) {
+    return new Promise((resolve, reject) => {
+      NativeModules.RNRandomBytes.randomBytes(length, function(err, base64) {
+        if (err) {
+          reject(err);
+        } else {
+          const result = base64js.toByteArray(base64).buffer;
+          resolve(result);
+        }
+      });
+    });
+  }
 };
