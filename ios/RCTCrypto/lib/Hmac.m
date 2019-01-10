f510e7a8293ad1703fc4066de0662ac7bbc83e93
Pedro Gomes <pedrogomes94@gmail.com>
2019-01-10 20:30:12 +0000

    accept hmac input in hex encoding

--- ios/RCTCrypto/lib/Hmac.m
+++ ios/RCTCrypto/lib/Hmac.m
@@ -8,7 +8,7 @@
 
 + (NSString *) hmac256: (NSString *)input key: (NSString *)key {
     NSData *keyData = [Shared fromHex:key];
-    NSData* inputData = [input dataUsingEncoding:NSUTF8StringEncoding];
+    NSData* inputData = [Shared fromHex:input];
     void* buffer = malloc(CC_SHA256_DIGEST_LENGTH);
     CCHmac(kCCHmacAlgSHA256, [keyData bytes], [keyData length], [inputData bytes], [inputData length], buffer);
     NSData *nsdata = [NSData dataWithBytesNoCopy:buffer length:CC_SHA256_DIGEST_LENGTH freeWhenDone:YES];
